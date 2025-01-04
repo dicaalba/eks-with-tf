fba191888eb7cd59728ffb676cd9e8127c8393bb
Diana Alfaro <dicaalba@gmail.com>
2025-01-04 14:21:00 -0500

    feat: update sg to ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -67,7 +67,8 @@ resource "aws_iam_instance_profile" "bastion_instance_profile" {
 resource "aws_instance" "bastion" {
   ami                    = var.ami_id  # AMI ID (pasado como variable)
   instance_type           = var.instance_type  # Tipo de instancia (pasado como variable)
-  security_groups        = [aws_security_group.bastion_sg.id]  # Asociamos el Security Group
+  vpc_security_group_ids = [aws_security_group.default.id]
+  # Asociamos el Security Group
   iam_instance_profile   = aws_iam_instance_profile.bastion_instance_profile.name  # Perfil IAM para permisos
   associate_public_ip_address = true  # Asignar IP pública
 
